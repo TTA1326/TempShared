f448f93f8269a5bee41defc419b1bbb11d29fce4
TTA1326 <45601381+TTA1326@users.noreply.github.com>
2020-12-16 15:27:34 -0600

    Create ISqlConnectionStringBuilderEx.cs

---
+++ ISqlConnectionStringBuilderEx.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.Interfaces
+{
+    public interface ISqlConnectionStringBuilderEx
+    {
+        string ConnectionString();
+
+        string MaskedConnectionString();
+
+        object this[string keyword] { get; }
+    }
+}
